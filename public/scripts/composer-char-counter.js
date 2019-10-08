2bdfb35d45145d4b925607f31092b74fa0a1819c
Tammie Chung <tammie.chung11@gmail.com>
2019-10-08 15:37:43 +0000

    Alter code to incorporate IDs instead of classes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,15 @@
-$('.new-tweet textarea').on('keyup', function() {
-  const charsUsed = $(this).val().length;
-  const counter = $(this).nextAll('.counter');
-  counter.text(140 - charsUsed);
-  if (counter.text() < 0) {
-    counter.addClass('invalid');
-  } 
-  if (counter.text() >= 0) {
-    counter.removeClass('invalid');
-  }
+$(window).on('load', function() {
+  
+  $('#new-tweet textarea').on('keyup', function() {
+    const charsUsed = $(this).val().length;
+    const counter = $(this).nextAll('.counter');
+    counter.text(140 - charsUsed);
+    if (counter.text() < 0) {
+      counter.addClass('invalid');
+    } 
+    if (counter.text() >= 0) {
+      counter.removeClass('invalid');
+    }
+  });
+
 });
